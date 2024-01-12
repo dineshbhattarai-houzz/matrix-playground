a4376cd47d69bb2681da71134aa1e36d7f6e8d35
dineshbhattarai-houzz <dinesh.bhattarai@houzz.com>
2024-01-12 16:42:21 +0545

    feat(express): add error handler

--- oidc-companion/index.ts
+++ oidc-companion/index.ts
@@ -7,6 +7,12 @@ const oidcProvider = getProvider(OIDC_ISSUER);
 const app = express();
 app.use("/oidc", oidcProvider.callback());
 
+app.use(function(err, _req, res, _next) {
+  res.status(err.status || 500);
+  console.error(err.message, err)
+});
+
+
 app.listen(PORT, () => {
   console.info(
     `oidc-provider listening on port ${PORT}, check http://localhost:${PORT}/oidc/.well-known/openid-configuration`,
